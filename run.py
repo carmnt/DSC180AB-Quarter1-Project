9d686001e74e14d33c17c4cd5c32484524813959
Kathleen Nguyen <129817006+knguyen64@users.noreply.github.com>
2024-11-04 22:14:51 -0800

    add mask function

--- run.py
+++ run.py
@@ -2,6 +2,12 @@
 
 import sys
 import json
+import pandas as pd
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import CrossEntropyLoss
+from torch.nn.utils.rnn import pad_sequence
+import random
 
 from etl import get_data
 
@@ -13,3 +19,17 @@ def main(targets):
 
 if __name__ == '__main__':
     args = sys.argv[1:]
+
+def mask(seq, mask=0.15):
+    '''Masks percentage of positions on a dna sequence'''
+    seq_len = len(seq)
+    num_mask = int(seq_len * mask)
+    positions_to_mask = random.sample(range(seq_len), num_mask)
+
+    # Masks positions on sequence
+    seq_list = list(seq)
+    for pos in positions_to_mask:
+        seq_list[pos] = '?'
+
+    masked_sequence = ''.join(seq_list)
+    return masked_sequence

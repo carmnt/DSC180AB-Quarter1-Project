685e45a6b133b86c3366016ebb7d45928882106b
Kathleen Nguyen <129817006+knguyen64@users.noreply.github.com>
2024-11-17 15:27:41 -0800

    add gpn model

--- run.py
+++ run.py
@@ -3,11 +3,6 @@
 import sys
 import json
 import pandas as pd
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.nn import CrossEntropyLoss
-from torch.nn.utils.rnn import pad_sequence
-import random
 
 from etl import get_data
 
@@ -19,17 +14,3 @@ def main(targets):
 
 if __name__ == '__main__':
     args = sys.argv[1:]
-
-def mask(seq, mask=0.15):
-    '''Masks percentage of positions on a dna sequence'''
-    seq_len = len(seq)
-    num_mask = int(seq_len * mask)
-    positions_to_mask = random.sample(range(seq_len), num_mask)
-
-    # Masks positions on sequence
-    seq_list = list(seq)
-    for pos in positions_to_mask:
-        seq_list[pos] = '?'
-
-    masked_sequence = ''.join(seq_list)
-    return masked_sequence
